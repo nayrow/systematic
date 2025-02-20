96216d762ad4007c5b58124bb49fe0c6f5bdd57c
nayrow <paseresebastian0504@gmail.com>
2025-02-20 21:46:53 +0200

    finished team page

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,40 +1,57 @@
 'use client'
 
-import Header from "@/app/components/header";
+import Header from "@/app/components/header/header";
 import Hero from "@/app/team/components/hero";
-import Image from "next/image";
+import Card from "@/app/components/card";
 
 export default function Page(){
     return (
         <div>
             <Header/>
             <Hero/>
-            <div className="w-full my-24">
+            <div className="w-full my-24 space-y-24">
                 <div className="flex gap-48 justify-center">
-                    <div className="w-1/5 h-[400px] relative flex flex-col justify-end">
-                        <Image
-                            alt="Scenic view"
-                            src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-                            width={2830}
-                            height={1500}
-                            className="absolute inset-0 -z-10 object-cover w-full h-full rounded-xl"
-                            priority
-                        />
-                        <div className={"w-full my-8 flex flex-col gap-2 items-center"}>
-                            <p className={"text-2xl font-bold"}>Mentor</p>
-                            <p>Andrei Mario IVAN</p>
-                        </div>
+                    <Card
+                        title={"Mentor"}
+                        subtitle={"Andrei Mario IVAN"}
+                        image={"https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"}
+                    />
+                    <Card
+                        title={"Team Leader"}
+                        subtitle={"Ana ACSINTE"}
+                        image={"https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"}
+                    />
+                </div>
+                <div>
+                    <div className={"py-8 bg-primary"}>
+                        <p className={"text-4xl font-bold text-center"}>
+                            Departments
+                        </p>
                     </div>
-                    <div className="w-1/5  h-[400px] relative">
-                        <Image
-                            alt="Scenic view"
-                            src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-                            width={2830}
-                            height={1500}
-                            className="absolute inset-0 -z-10 object-cover w-full h-full rounded-xl"
-                            priority
+                    <div className="p-24 w-full flex flex-wrap justify-center gap-x-48 gap-y-24">
+                    <Card
+                            title="Proiectare"
+                            href="team/avioane"
+                            image="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+                        />
+                        <Card
+                            title="Mecanica"
+                            image="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+                        />
+                        <Card
+                            title="Programare"
+                            image="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+                        />
+                        <Card
+                            title="Design"
+                            image="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+                        />
+                        <Card
+                            title="PR"
+                            image="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
                         />
                     </div>
+
                 </div>
             </div>
         </div>
