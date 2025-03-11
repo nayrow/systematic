40311365cde7f2934f33784bbf500e16e52490e3
nayrow <112016631+nayrow@users.noreply.github.com>
2025-03-11 18:05:06 +0200

    Update page.tsx

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -18,7 +18,7 @@ export default async function Page() {
                             key={leader.id}
                             title={leader.role}
                             subtitle={leader.name}
-                            image={"https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"}
+                            image={leader.image.url}
                         />
                     ))}
                 </div>
